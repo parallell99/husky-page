d6b9a3e26dc2b84041cdd9f52d7e0fa051232f09
parallell99 <jr.projectx@gmail.com>
2025-12-28 14:20:00 +0700

    Add: integrate new UI components including ArticleSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/navbar.jsx'
 import Hero from './components/Hero.jsx'  
 import Footer from './components/Footer.jsx'
+import ArticleSection from './components/ArticleSection.jsx'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
     <Navbar />
     <Hero />
+    <ArticleSection />
     <Footer />
     </>
   )
---
+++ src/components/ArticleSection.jsx
@@ -0,0 +1,75 @@
+import { SearchIcon } from "lucide-react";
+
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { Button } from "@/components/ui/button";
+
+function ArticleSection() {
+  return (
+    <>
+      <div className=" my-10 lg:mx-20">
+        <p className="text-2xl font-semibold pl-3 py-4">Latest articles</p>
+        <div className="flex flex-col justify-center items-center w-full   bg-brown-200 lg:flex-row-reverse lg:h-[80px] lg:rounded-2xl lg:px-5">
+          <div className="grid w-[343px] max-w-sm lg:w-[560px] bg-white">
+            <InputGroup>
+              <InputGroupInput
+                placeholder="Search"
+                className="text-sm font-medium text-brown-600 w-full"
+              />
+              <InputGroupAddon>
+                <SearchIcon />
+              </InputGroupAddon>
+            </InputGroup>
+          </div>
+          <div className="flex flex-col justify-start items-start w-full pl-3">
+            <p className="text-sm font-medium text-brown-400 lg:hidden pt-3 pb-1">
+              Category
+            </p>
+          </div>
+          <div className="lg:hidden bg-white">
+            <Select>
+              <SelectTrigger className="w-[343px]">
+                <SelectValue
+                  placeholder="Highlight"
+                  className="text-sm font-bold text-brown-400"
+                />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Fruits</SelectLabel>
+                  <SelectItem value="apple">Apple</SelectItem>
+                  <SelectItem value="banana">Banana</SelectItem>
+                  <SelectItem value="blueberry">Blueberry</SelectItem>
+                  <SelectItem value="grapes">Grapes</SelectItem>
+                  <SelectItem value="pineapple">Pineapple</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className="flex max-lg:hidden gap-2">
+            <Button className="hover:bg-brown-300 text-brown-500 cursor-pointer">Highlight</Button>
+            <Button className="hover:bg-brown-300 text-brown-500 cursor-pointer">Cat</Button>
+            <Button className="hover:bg-brown-300 text-brown-500 cursor-pointer">Inspiration</Button>
+            <Button className="hover:bg-brown-300 text-brown-500 cursor-pointer">Ganeral</Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ArticleSection;
